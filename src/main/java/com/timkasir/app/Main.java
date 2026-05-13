1b42e7da35489d359d07a60da59804b984c6690a
jaee24 <dyngpoo@gmail.com>
2026-05-13 12:36:42 +0700

    add db connection

--- src/main/java/com/timkasir/app/Main.java
+++ src/main/java/com/timkasir/app/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import com.timkasir.database.DBConnection;
 
 public class Main extends Application {
 
